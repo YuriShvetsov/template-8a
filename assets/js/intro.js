8de2a5cd34a85c4bf2847bdf1f932ad73434053b
YuriShvetsov <shvetsov.yuri.new@gmail.com>
2019-05-12 10:49:47 +0500

    adaptive

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,6 +1,6 @@
-// Slick slider.
-
 $(document).ready(function () {
+
+    /* Slider */
     $('.slider__box').on('init', function (event, slick) {
         var $items = slick.$dots.find('li');
         $items.addClass('transparent-circle');
@@ -15,27 +15,34 @@ $(document).ready(function () {
         dotsClass: 'slider__dots',
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        // responsive: [
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             arrows: false,
-        //             centerMode: true,
-        //             centerPadding: '40px',
-        //             slidesToShow: 3
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 480,
-        //         settings: {
-        //             arrows: false,
-        //             centerMode: true,
-        //             centerPadding: '40px',
-        //             slidesToShow: 1
-        //         }
-        //     }
-        // ]
+        slidesToScroll: 1
     });
+
+    /* Menu */
+    const hamburger = $('.hamburger');
+    const menu = $('.header__nav-list');
+    const menu_links = $('.nav__link');
+
+    hamburger.on('click', function(e) {
+        e.preventDefault;
+        $(this).toggleClass('hamburger_active');
+        menu.toggle('slow');
+    });
+
+    $(document).mouseup(function (e) { // событие клика по веб-документу
+        e.preventDefault;
+        if (!$('.hamburger').is('.hamburger_active')) {
+            return;
+        }
+        if (!menu.is(e.target) // если клик был не по нашему блоку
+            && menu.has(e.target).length === 0 // и не по его дочерним элементам
+            && !hamburger.is(e.target) // не по контейнеру кнопки меню
+            && hamburger.has(e.target).length === 0 // и не по его дочерним элементам
+            || menu_links.is(e.target)) { // или при клике по якорям
+            menu.hide(); // то скрываем меню
+            hamburger.removeClass('hamburger_active'); // и возвращаем кнопке меню класс по дефолту
+        }
+    });
+    
 });
 
